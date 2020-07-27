e8921e764ac91c6e89c12c6ee4d5a7e025176e19
komenchuk <komenchuk77@gmail.com>
2020-07-27 11:16:18 +0300

    Add project

--- project/sqlighter.py
+++ project/sqlighter.py
@@ -1,29 +1,27 @@
 import sqlite3
 
-
 class SQLighter:
 
-    def __init__(self, database_file):
+    def __init__(self, database):
         """Подключаемся к БД и сохраняем курсор соединения"""
-        self.connection = sqlite3.connect(database_file)
+        self.connection = sqlite3.connect(database)
         self.cursor = self.connection.cursor()
 
-    def get_subscriptions(self, status=True):
+    def get_subscriptions(self, status = True):
         """Получаем всех активных подписчиков бота"""
         with self.connection:
             return self.cursor.execute("SELECT * FROM `subscriptions` WHERE `status` = ?", (status,)).fetchall()
 
     def subscriber_exists(self, user_id):
-        """Проверяем есть ли юзер в базе"""
+        """Проверяем, есть ли уже юзер в базе"""
         with self.connection:
-            result = self.cursor.execute("SELECT * FROM `subscriptions` WHERE `user_id` = ?", (user_id,)).fetchall()
+            result = self.cursor.execute('SELECT * FROM `subscriptions` WHERE `user_id` = ?', (user_id,)).fetchall()
             return bool(len(result))
 
-    def add_subscriber(self, user_id, status=True):
+    def add_subscriber(self, user_id, status = True):
         """Добавляем нового подписчика"""
         with self.connection:
-            return self.cursor.execute("INSERT INTO `subscriptions` (`user_id`, `status`) VALUES (?, ?)",
-                                       (user_id, status))
+            return self.cursor.execute("INSERT INTO `subscriptions` (`user_id`, `status`) VALUES(?,?)", (user_id,status))
 
     def update_subscription(self, user_id, status):
         """Обновляем статус подписки пользователя"""
